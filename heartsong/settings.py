cc585e7fe596b64ae0b425ccf2e453e4939c514a
kongtianyi <598515595@qq.com>
2016-10-15 10:56:31 +0800

    Update settings.py

--- heartsong/settings.py
+++ heartsong/settings.py
@@ -7,8 +7,6 @@ NEWSPIDER_MODULE = 'heartsong.spiders'
 
 ROBOTSTXT_OBEY = False  # 不遵守Robot协议
 
-# 配置管道，数字代表优先级，因为本项目只有一个管道，所以可取1-1000中的任意值
-ITEM_PIPELINES = {
-    'heartsong.pipelines.HeartsongPipeline': 300,
-}
+# 使用transCookie.py翻译出的Cookie字典
+COOKIE = {'key1': 'value1', 'key2': 'value2'}
 
