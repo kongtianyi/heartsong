a6612cdb2db635b771eee144a1c473c248b7a317
kongtianyi <598515595@qq.com>
2016-10-15 10:55:43 +0800

    Update items.py

--- heartsong/items.py
+++ heartsong/items.py
@@ -3,8 +3,4 @@
 import scrapy
 
 class HeartsongItem(scrapy.Item):
-    title = scrapy.Field()  # 帖子的标题
-    url = scrapy.Field()  # 帖子的网页链接
-    author = scrapy.Field()  # 帖子的作者
-    post_time = scrapy.Field()  # 发表时间
-    content = scrapy.Field()  # 帖子的内容
+    pass
